966cbcae895e32a7e98cadc69bd11079b08ada43
its-man-ish <67169163+its-man-ish@users.noreply.github.com>
2021-08-26 11:45:47 +0530

    Added model for more info

--- src/views/Products.vue
+++ src/views/Products.vue
@@ -4,10 +4,10 @@
     <div class="pa-1 ma-5">
       <v-row class="">
 
-        <v-col cols="12" md="3" sm="6" xs="6">
+        <v-col cols="12" md="3" sm="6" xsm="6">
           <v-hover v-slot="{ hover }">
              <v-card class="mx-auto" color="grey lighten-4">
-              <v-img :aspect-ratio="16/20"
+              <v-img :aspect-ratio="13/16"
                src="https://assets.myntassets.com/h_1440,q_100,w_1080/v1/assets/images/1700944/2019/6/8/972c9498-3a37-4d5d-976c-4493b4d5c0021559989322793-HRX-by-Hrithik-Roshan-Men-Yellow-Printed-Round-Neck-T-Shirt--1.jpg"
               >
                   <v-expand-transition>
@@ -18,156 +18,121 @@
                   </div>
                  </v-expand-transition>
               </v-img>
-             <v-card-text class="pt-6" style="position: relative;">
-              <v-btn absolute color="yellow" class="black--text" fab right top>
-                <v-icon>edit</v-icon>
-              </v-btn>
-              <p class="font-weight-light orange--text mb-2">
-                <b>HRX by Hrithik Roshan</b>
-              </p>
-            </v-card-text>
+
+            <v-card-title>HRX by Hrithik Roshan</v-card-title>
+
+
+             <v-card-text>
+
+               <v-chip-group
+                 v-model="selection"
+                 active-class="deep-purple accent-4 white--text"
+                 column
+               >
+                 <v-chip color="success">Green</v-chip>
+
+                 <v-chip color="blue">Blue</v-chip>
+
+                 <v-chip color="yellow">Yellow</v-chip>
+
+                 <v-chip color="purple">Purple</v-chip>
+               </v-chip-group>
+             </v-card-text>
+
+               <v-card-actions>
+
+                  <v-btn text>status: Active</v-btn>
+                  <v-spacer> </v-spacer>
+
+
+                 <v-dialog
+                   v-model="dialog"
+                   width="500"
+                 >
+                   <template v-slot:activator="{ on, attrs }">
+                     <v-btn
+                       color="black lighten-2"
+                       dark
+                       elevation="1"
+                       v-bind="attrs"
+                       v-on="on"
+                       icon
+                       class="yellow"
+                       style="width:50px; height:50px"
+                     >
+                      <v-icon size="30">mdi-pencil-box-outline</v-icon>
+                     </v-btn>
+                   </template>
+
+                   <v-card>
+                     <v-card-title class="text-h5 grey lighten-2">
+                    HRX by Hrithik Roshan
+                     </v-card-title>
+
+                     <template>
+                        <v-carousel
+                          cycle
+                          height="400"
+                          hide-delimiter-background
+                          show-arrows-on-hover
+                        >
+                          <v-carousel-item >
+                            <v-sheet
+                              color="yellow"
+                              height="100%"
+                            >
+                              <v-row
+                                class="fill-height"
+                                align="center"
+                                justify="center"
+                              >
+                                <div class="text-h2">
+
+                                  Image 1
+                                </div>
+                              </v-row>
+                            </v-sheet>
+                          </v-carousel-item>
+
+                          <v-carousel-item >
+                            <v-sheet
+                              color="green"
+                              height="100%"
+                            >
+                              <v-row
+                                class="fill-height"
+                                align="center"
+                                justify="center"
+                              >
+                                <div class="text-h2">
+                                  Image2
+                                </div>
+                              </v-row>
+                            </v-sheet>
+                          </v-carousel-item>
+
+                        </v-carousel>
+                      </template>
+                     <v-card-text>
+                       <b>Description</b>
+                     </v-card-text>
+                     <v-divider></v-divider>
+                     <v-card-actions>
+                       <v-spacer></v-spacer>
+                       <v-btn
+                         color="primary"
+                         text
+                         @click="dialog = false"
+                       >
+                         I accept
+                       </v-btn>
+                     </v-card-actions>
+                   </v-card>
+                 </v-dialog>
+              </v-card-actions>
            </v-card>
         </v-hover>
        </v-col>
-
-
-       <v-col cols="12" md="3" sm="6" xs="6">
-         <v-hover v-slot="{ hover }">
-            <v-card class="mx-auto" color="grey lighten-4">
-             <v-img :aspect-ratio="16/20"
-              src="https://assets.myntassets.com/dpr_1.5,q_60,w_400,c_limit,fl_progressive/assets/images/productimage/2020/9/16/25f5b6a7-ce25-4e66-8a61-3aa2037b6c741600207614629-1.jpg"
-             >
-                 <v-expand-transition>
-                 <div v-if="hover" class="d-flex transition-fast-in-fast-out yellow darken-2 v-card--reveal text-h2 white--text"   style="height: 100%;" >
-                  <v-card height="100">
-                    <img src="https://assets.myntassets.com/dpr_1.5,q_60,w_400,c_limit,fl_progressive/assets/images/productimage/2020/1/17/d71045b2-4300-4395-b110-f652fa00e9e51579211471795-1.jpg" alt="" style="width:100%;">
-                  </v-card>
-                 </div>
-                </v-expand-transition>
-             </v-img>
-            <v-card-text class="pt-6" style="position: relative;">
-             <v-btn absolute color="yellow" class="black--text" fab right top>
-               <v-icon></v-icon>
-             </v-btn>
-             <p class="font-weight-light orange--text mb-2">
-               <b>HRX by Hrithik Roshan</b>
-             </p>
-           </v-card-text>
-          </v-card>
-       </v-hover>
-      </v-col>
-
-
-      <v-col cols="12" md="3" sm="6" xs="6">
-        <v-hover v-slot="{ hover }">
-           <v-card class="mx-auto" color="grey lighten-4">
-            <v-img :aspect-ratio="16/20"
-             src="https://assets.myntassets.com/dpr_1.5,q_60,w_400,c_limit,fl_progressive/assets/images/13719116/2021/3/3/90d0980a-8b39-48bd-9bf9-a67beb14a9741614753528611-Athena-Fuchsia-Pink-top-with-power-shoulder-sleeve-detail-22-1.jpg"
-            >
-                <v-expand-transition>
-                <div v-if="hover" class="d-flex transition-fast-in-fast-out yellow darken-2 v-card--reveal text-h2 white--text"   style="height: 100%;" >
-                 <v-card height="100">
-                   <img src="https://assets.myntassets.com/dpr_1.5,q_60,w_400,c_limit,fl_progressive/assets/images/13719116/2021/3/3/90d0980a-8b39-48bd-9bf9-a67beb14a9741614753528611-Athena-Fuchsia-Pink-top-with-power-shoulder-sleeve-detail-22-1.jpg" alt="" style="width:100%;">
-                 </v-card>
-                </div>
-               </v-expand-transition>
-            </v-img>
-           <v-card-text class="pt-6" style="position: relative;">
-            <v-btn absolute color="yellow" class="black--text" fab right top>
-              <v-icon>mdi-border-color</v-icon>
-            </v-btn>
-            <p class="font-weight-light orange--text mb-2">
-              <b>HRX by Hrithik Roshan</b>
-            </p>
-          </v-card-text>
-         </v-card>
-      </v-hover>
-      </v-col>
-
-
-      <v-col md="3" sm="6" xs="6">
-        <v-hover v-slot="{ hover }">
-           <v-card class="mx-auto" color="grey lighten-4">
-            <v-img :aspect-ratio="16/20"
-             src="https://assets.ajio.com/medias/sys_master/root/20210514/T01n/609e7c45f997ddb3129e999c/-473Wx593H-461571257-blue-MODEL.jpg"
-            >
-                <v-expand-transition>
-                <div v-if="hover" class="d-flex transition-fast-in-fast-out yellow darken-2 v-card--reveal text-h2 white--text"   style="height: 100%;" >
-                 <v-card height="100">
-                   <img src="https://assets.myntassets.com/h_1440,q_90,w_1080/v1/assets/images/1700944/2019/6/8/67f8fc06-3131-4ae9-a869-2b485f0aca3c1559989322742-HRX-by-Hrithik-Roshan-Men-Yellow-Printed-Round-Neck-T-Shirt--4.jpg" alt="" style="width:100%;">
-                 </v-card>
-                </div>
-               </v-expand-transition>
-            </v-img>
-           <v-card-text class="pt-6" style="position: relative;">
-            <v-btn absolute color="yellow" class="black--text" fab right top>
-              <v-icon>mdi-border-color</v-icon>
-            </v-btn>
-            <p class="font-weight-light orange--text mb-2">
-              <b>HRX by Hrithik Roshan</b>
-            </p>
-          </v-card-text>
-         </v-card>
-      </v-hover>
-     </v-col>
-
-
-
-        <v-col md="3" sm="6" xsm="6">
-         <v-hover>
-           <v-card flat class="grey lighten-3">
-            <v-img src="https://assets.ajio.com/medias/sys_master/root/20210514/T01n/609e7c45f997ddb3129e999c/-473Wx593H-461571257-blue-MODEL.jpg"></v-img>
-           </v-card>
-         </v-hover>
-        </v-col>
-
-        <v-col md="3" sm="6" xsm="6">
-          <v-card flat class="grey lighten-3 pa-2">
-           <v-img src="https://assets.myntassets.com/h_1440,q_100,w_1080/v1/assets/images/1700944/2019/6/8/972c9498-3a37-4d5d-976c-4493b4d5c0021559989322793-HRX-by-Hrithik-Roshan-Men-Yellow-Printed-Round-Neck-T-Shirt--1.jpg"></v-img>
-          </v-card>
-        </v-col>
-
-        <v-col md="3" sm="6" xsm="6">
-          <v-card flat class="grey lighten-3 pa-2">
-            <v-img src="https://oldnavy.gap.com/Asset_Archive/ONWeb/content/0025/833/538/assets/210614_65-M2305_M_DP_Sale.jpg"></v-img>
-          </v-card>
-        </v-col>
-
-        <v-col md="3" sm="6" xsm="6">
-          <v-card flat class="grey lighten-3 pa-2">
-            <v-img src="https://images.squarespace-cdn.com/content/v1/5442b6cce4b0cf00d1a3bef2/1622147341413-EBQTM7W3GDLI57A3X02B/fair-trade-mens-clothing-buck-mason.jpeg"></v-img>
-          </v-card>
-        </v-col>
-
-        <v-col md="3" sm="6" xsm="6">
-          <v-card flat class="grey lighten-3 pa-2">
-           <v-img src="https://assets.myntassets.com/dpr_1.5,q_60,w_400,c_limit,fl_progressive/assets/images/13719116/2021/3/3/90d0980a-8b39-48bd-9bf9-a67beb14a9741614753528611-Athena-Fuchsia-Pink-top-with-power-shoulder-sleeve-detail-22-1.jpg"></v-img>
-          </v-card>
-        </v-col>
-
-        <v-col md="3" sm="6" xsm="6">
-          <v-card flat class="grey lighten-3 pa-2">
-           <v-img src="https://www.coverstory.co.in/media/cms/home/category/work.jpg"></v-img>
-          </v-card>
-        </v-col>
-
-        <v-col md="3" sm="6" xsm="6">
-          <v-card flat class="grey lighten-3 pa-2">
-           <v-img src="https://assets.myntassets.com/dpr_1.5,q_60,w_400,c_limit,fl_progressive/assets/images/productimage/2020/1/17/d71045b2-4300-4395-b110-f652fa00e9e51579211471795-1.jpg"></v-img>
-          </v-card>
-        </v-col>
-
-        <v-col md="3" sm="6" xsm="6">
-          <v-card flat class="grey lighten-3 pa-2">
-           <v-img src="https://assets.myntassets.com/dpr_1.5,q_60,w_400,c_limit,fl_progressive/assets/images/productimage/2020/9/16/25f5b6a7-ce25-4e66-8a61-3aa2037b6c741600207614629-1.jpg"></v-img>
-          </v-card>
-        </v-col>
-
-        <v-col cols="12" md="3" sm="6" xsm="6">
-          <v-card flat class="grey lighten-3 pa-2">
-           <v-img src="https://assets.myntassets.com/h_1440,q_100,w_1080/v1/assets/images/1700944/2019/6/8/972c9498-3a37-4d5d-976c-4493b4d5c0021559989322793-HRX-by-Hrithik-Roshan-Men-Yellow-Printed-Round-Neck-T-Shirt--1.jpg"></v-img>
-          </v-card>
-        </v-col>
 
       </v-row>
     </div>
@@ -176,9 +141,31 @@
 
 
 
-  <script>  export default {
-      name: 'Products',
+<script>
 
-
+import axios from "axios"
+export default {
+  name:"Products",
+  data () {
+    return {
+      products: null,
+      dialog: false,
+      product_images: null,
     }
-  </script>
+  },
+
+
+/***** API CALL ***/
+mounted(){
+ axios
+ .get('http://localhost:3000/api/products')
+ .then(result => {
+   this.products = result.data
+   console.log(result.data)
+   this.products=result.data
+ })
+}
+
+
+}
+</script>
